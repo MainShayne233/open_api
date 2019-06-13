0ed26ecac55bd924a5ee711164b1f46875cfba28
MainShayne233 <shaynetremblay@gmail.com>
2019-06-13 17:19:43 -0400

    Assert response struct

--- test/open_api_test.exs
+++ test/open_api_test.exs
@@ -2,28 +2,38 @@ defmodule OpenAPITest do
   use ExUnit.Case
 
   setup_all do
-    defmodule MockAPI do
-      use OpenAPI,
-        json_decoder: &Jason.decode/1,
-        schema: %{
-          "servers" => [%{"url" => "https://api.mock.com"}],
-          "paths" => %{
-            "/health_check" => %{
-              "get" => %{"responses" => %{"200" => %{"content" => %{"text/plain" => %{}}}}}
-            },
-            "/users" => %{
-              "get" => %{
-                "responses" => %{
-                  "200" => %{
-                    "content" => %{
-                      "application/json" => %{
-                        "schema" => %{
-                          "type" => "array",
-                          "items" => %{
-                            "type" => "object",
-                            "properties" => %{
-                              "username" => %{"type" => "string"}
-                            }
+    Tesla.Mock.mock_global(fn
+      %Tesla.Env{url: "https://api.mock.com/health_check"} ->
+        %Tesla.Env{status: 200, body: "healthy!"}
+
+      %Tesla.Env{url: "https://api.mock.com/users"} ->
+        %Tesla.Env{status: 200, body: "[{\"username\":\"jane\"}]"}
+    end)
+
+    :ok
+  end
+
+  defmodule MockAPI do
+    use OpenAPI,
+      json_decoder: &Jason.decode/1,
+      schema: %{
+        "servers" => [%{"url" => "https://api.mock.com"}],
+        "paths" => %{
+          "/health_check" => %{
+            "get" => %{"responses" => %{"200" => %{"content" => %{"text/plain" => %{}}}}}
+          },
+          "/users" => %{
+            "get" => %{
+              "responses" => %{
+                "200" => %{
+                  "content" => %{
+                    "application/json" => %{
+                      "schema" => %{
+                        "type" => "array",
+                        "items" => %{
+                          "type" => "object",
+                          "properties" => %{
+                            "username" => %{"type" => "string"}
                           }
                         }
                       }
@@ -34,17 +44,7 @@ defmodule OpenAPITest do
             }
           }
         }
-    end
-
-    Tesla.Mock.mock_global(fn
-      %Tesla.Env{url: "https://api.mock.com/health_check"} ->
-        %Tesla.Env{status: 200, body: "healthy!"}
-
-      %Tesla.Env{url: "https://api.mock.com/users"} ->
-        %Tesla.Env{status: 200, body: "[{\"username\":\"jane\"}]"}
-    end)
-
-    :ok
+      }
   end
 
   describe "using/2" do
@@ -58,7 +58,7 @@ defmodule OpenAPITest do
     end
 
     test "should generate the /users path code" do
-      assert {:ok, [%{username: "jane"}]} =
+      assert {:ok, [%OpenAPITest.MockAPI.Users.Get.Response.Status200.Item{username: "jane"}]} =
                __MODULE__.MockAPI.Users.Get.make_request(tesla_adapter: Tesla.Mock)
     end
   end
